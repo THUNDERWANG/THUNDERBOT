afc1cd9590346d646e81d769b9b905dbb4ee63ca
Jason Wang <crapitsjason@gmail.com>
2020-12-20 20:29:36 -0800

    Update call.js

--- commands/call.js
+++ commands/call.js
@@ -3,19 +3,21 @@ module.exports = {
     description: 'clears a temp role, adds online-only users to a temp role, pings role',
     async mentionRole(roleID, tempRoleID, message) {
         try {
-            const role = await message.guild.roles.fetch(roleID);
-            role.members.forEach(async member => {
-                if (member.presence.status === 'online') await member.roles.add(tempRoleID);
+            const roleMembers = await message.guild.roles.fetch(roleID);
+            const rolePromises = roleMembers.members.map(member => {
+                if (member.presence.status === 'online') member.roles.add(tempRoleID);
             });
-            await message.channel.send(`<@&${tempRoleID}>`);
-            const tempRole = await message.guild.roles.fetch(tempRoleID);
-            tempRole.members.forEach(async member => await member.roles.remove(tempRoleID));
+            await Promise.all(rolePromises);
+            message.channel.send(`<@&${tempRoleID}>`);
+            const tempMembers = await message.guild.roles.fetch(tempRoleID);
+            const tempPromises = tempMembers.members.map(member => member.roles.remove(tempRoleID));
+            await Promise.all(tempPromises);
         } catch (error) {
             console.log(error)
         };
     },
-    execute(message, args) {
-        if (args[0].toLowerCase() === 'xmage') this.mentionRole('585313738169778177', '790139800086118430', message) 
-        else if(args[0].toLowerCase() === 'cock') this.mentionRole('585314755976364043', '790129809794531328', message)
+    async execute(message, args) {
+        if (args[0].toLowerCase() === 'xmage') await this.mentionRole('789944500386529300', '789443609178800189', message) 
+        else if(args[0].toLowerCase() === 'cock') await this.mentionRole('585314755976364043', '790129809794531328', message)
     }
 };
