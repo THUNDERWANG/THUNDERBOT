a9b1f6b37ba2c3fd1b3b379dbfb769b5739c6af5
Jason Wang <crapitsjason@gmail.com>
2020-12-20 18:39:14 -0800

    Update call.js

--- commands/call.js
+++ commands/call.js
@@ -3,13 +3,13 @@ module.exports = {
     description: 'clears a temp role, adds online-only users to a temp role, pings role',
     async mentionRole(roleID, tempRoleID, message) {
         try {
-            const role = await message.guild.roles.fetch(roleID, false, true);
+            const role = await message.guild.roles.fetch(roleID);
             role.members.forEach(async member => {
                 if (member.presence.status === 'online') await member.roles.add(tempRoleID);
             });
             await message.channel.send(`<@&${tempRoleID}>`);
-            const tempRole = await message.guild.roles.fetch(tempRoleID, false, true);
-            tempRole.members.forEach(async member => member.roles.remove(tempRoleID));
+            const tempRole = await message.guild.roles.fetch(tempRoleID);
+            tempRole.members.forEach(async member => await member.roles.remove(tempRoleID));
         } catch (error) {
             console.log(error)
         };
