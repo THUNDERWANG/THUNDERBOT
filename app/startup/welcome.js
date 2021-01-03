b6d67b17bcbab32c0f41136d4e35f38bf0eb5ea2
Jason Wang <crapitsjason@gmail.com>
2021-01-02 18:15:39 -0800

    Index added try/catch

--- app/startup/welcome.js
+++ app/startup/welcome.js
@@ -1,4 +1,4 @@
-module.exports.welcome = client => {
+module.exports.welcome = async member => {
 	const m1 = 'may you topdeck the timely wrath :beers:';
 	const m2 = 'may your P1P1s always be bombs :beers:';
 	const m3 = 'may you topdeck the lethal bolt :beers:';
@@ -9,15 +9,13 @@ module.exports.welcome = client => {
 	const welcomeId = '659117988405837832';
 	const germId = '585662276212621333';
 
-	client.on('guildMemberAdd', async member => {
-		try {
-			const index = Math.floor(Math.random() * messages.length);
-			const welcomeMessage = `Welcome <@${member.id}>, ` + messages[index];
-			const channel = member.guild.channels.cache.get(welcomeId);
-			await channel.send(welcomeMessage);
-			await member.roles.add(germId);
-		} catch(error) {
-			console.error(error);
-		}
-	});
+	try {
+		const index = Math.floor(Math.random() * messages.length);
+		const welcomeMessage = `Welcome <@${member.id}>, ` + messages[index];
+		const channel = member.guild.channels.cache.get(welcomeId);
+		await channel.send(welcomeMessage);
+		await member.roles.add(germId);
+	} catch(error) {
+		console.error(error);
+	}
 };
--- index.js
+++ index.js
@@ -12,48 +12,58 @@ for (const file of commandFiles) {
 	const command = require(`./app/commands/${file}`);
 	discordClient.commands.set(command.name, command);
 }
-
 const cooldowns = new Discord.Collection();
-db.sequelize.sync().then(()=>console.log('DB ready'));
 
-discordClient.once('ready', () => {
-	console.log('Bzz, THUNDERBOT is ready!');
+discordClient.once('ready', async () => {
+	try {
+		await db.sequelize.sync().then(()=>console.log('DB ready'));
+		console.log('Bzz, THUNDERBOT is ready!');
+	} catch(error) {
+		console.error(error);
+	}
 });
 
-welcome(discordClient);
+discordClient.on('guildMemberAdd', async member => {
+	try {
+		await welcome(member);
+	} catch(error) {
+		console.error(error);
+	}
+});
 
 discordClient.on('message', async message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
-	const args = message.content.toLowerCase().slice(prefix.length).split(/ +/);
-	const commandName = args.shift().toLowerCase();
+	try {
+		if (!message.content.startsWith(prefix) || message.author.bot) return;
+		const args = message.content.toLowerCase().slice(prefix.length).split(/ +/);
+		const commandName = args.shift().toLowerCase();
 
-	const command = discordClient.commands.get(commandName) || discordClient.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-	if (!command) return;
-	if (command.args && !args.length) {
-		return message.channel.send(`Did you mean ${process.env.PREFIX}${command.name} ${command.usage}?`);
+		const command = discordClient.commands.get(commandName) || discordClient.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+		if (!command) return;
+		if (command.args && !args.length) {
+			return message.channel.send(`Did you mean ${process.env.PREFIX}${command.name} ${command.usage}?`);
 		// } else if (!command.arg && args.length) {
 		//	return message.channel.send('This command does not require arguments.');
-	}
-	if (command.guildOnly && message.channel.type === 'dm') {
-		return message.channel.send('This command can only be used in the server!');
-	}
-	if (!cooldowns.has(commandName)) {
-		cooldowns.set(commandName, new Discord.Collection());
-	}
-	const timestamps = cooldowns.get(commandName);
-	if (!timestamps.has(message.author.id)) {
-		const cooldown = (command.cooldown || 0) * 1000;
-		timestamps.set(message.author.id, Date.now() + cooldown);
-		setTimeout(() => { timestamps.delete(message.author.id); }, cooldown);
-	} else {
-		const expirationTime = timestamps.get(message.author.id);
-		const time = Date.now();
-		if (expirationTime > Date.now()) {
-			return message.channel.send(`<@${message.author.id}>, please wait ${Math.ceil((expirationTime - time) / 1000)} seconds`);
 		}
-	}
-	try {
+		if (command.guildOnly && message.channel.type === 'dm') {
+			return message.channel.send('This command can only be used in the server!');
+		}
+		if (!cooldowns.has(commandName)) {
+			cooldowns.set(commandName, new Discord.Collection());
+		}
+		const timestamps = cooldowns.get(commandName);
+		if (!timestamps.has(message.author.id)) {
+			const cooldown = (command.cooldown || 0) * 1000;
+			timestamps.set(message.author.id, Date.now() + cooldown);
+			setTimeout(() => { timestamps.delete(message.author.id); }, cooldown);
+		} else {
+			const expirationTime = timestamps.get(message.author.id);
+			const time = Date.now();
+			if (expirationTime > Date.now()) {
+				return message.channel.send(`<@${message.author.id}>, please wait ${Math.ceil((expirationTime - time) / 1000)} seconds`);
+			}
+		}
 		command.execute(message, args);
+
 	} catch (error) {
 		console.error(error);
 		message.reply('could not execute that command!');
