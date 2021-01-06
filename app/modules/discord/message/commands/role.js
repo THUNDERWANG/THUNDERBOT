3889b48062c90bb9b56a596f11a1dd09f088b45d
Jason Wang <jasonywang0@gmail.com>
2021-01-06 12:01:19 -0800

    add confirmation to role.js

--- app/modules/discord/message/commands/role.js
+++ app/modules/discord/message/commands/role.js
@@ -10,28 +10,19 @@ module.exports = {
 		try {
 			if (args[0] === 'add') {
 				if (args[1] === 'xmage') {
-					if (!message.member.roles.cache.has(xmageId)) {
-						await message.member.roles.add(xmageId);
-						await message.channel.send(`<@${message.author.id}> has added <@&${xmageId}>`, { allowedMentions: { parse: [] } });
-					}
+					await message.member.roles.add(xmageId);
+					await message.channel.send(`<@${message.author.id}> added <@&${xmageId}>`, { allowedMentions: { parse: [] } });
 				} else if (args[1] === 'trice' || args[1] === 'cockatrice') {
-					if (!message.member.roles.cache.has(triceId)) {
-						await message.member.roles.add(triceId);
-						await message.channel.send(`<@${message.author.id}> has added <@&${triceId}>`, { allowedMentions: { parse: [] } });
-					}
+					await message.member.roles.add(triceId);
+					await message.channel.send(`<@${message.author.id}> added <@&${triceId}>`, { allowedMentions: { parse: [] } });
 				}
 			} else if (args[0] === 'remove') {
 				if (args[1] === 'xmage') {
-					if (message.member.roles.cache.has(xmageId)) {
-						await message.member.roles.remove(xmageId);
-						await message.channel.send(`<@${message.author.id}> has removed <@&${xmageId}>`, { allowedMentions: { parse: [] } });
-					}
-
+					await message.member.roles.remove(xmageId);
+					await message.channel.send(`<@${message.author.id}> removed <@&${xmageId}>`, { allowedMentions: { parse: [] } });
 				} else if (args[1] === 'trice' || args[1] === 'cockatrice') {
-					if (message.member.roles.cache.has(triceId)) {
-						await message.member.roles.remove(triceId);
-						await message.channel.send(`<@${message.author.id}> has removed <@&${triceId}>`, { allowedMentions: { parse: [] } });
-					}
+					await message.member.roles.remove(triceId);
+					await message.channel.send(`<@${message.author.id}> removed <@&${triceId}>`, { allowedMentions: { parse: [] } });
 				}
 			}
 		} catch (error) {
