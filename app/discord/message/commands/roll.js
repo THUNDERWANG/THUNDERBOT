6ec1034f15c54bb0c6c39d95b1b9e62af91bbeed
THUNDERWANG <crapitsjason@gmail.com>
2021-01-18 02:31:02 -0800

    add different roll command syntaxes

--- app/discord/message/commands/roll.js
+++ app/discord/message/commands/roll.js
@@ -2,15 +2,29 @@ module.exports = {
 	name: 'roll',
 	aliases: ['die', 'rng'],
 	args: true,
-	usage: ['[some number]'],
+	usage: ['[d100 or 3d100]'],
+	cooldown: 3,
 	description: 'generate a number from 1-X',
 	async execute(message, args) {
 		try {
-			if (parseInt(args[0])) {
-				const number = Math.floor(Math.random() * args[0]) + 1;
-				message.channel.send(`<@${message.author.id}> rolled a ${number}`, { allowedMentions: { parse: [] } });
+			if (args.length > 1) return;
+			let reply = 'Invalid argument. The max number of rolls is 10 and the max die size is 100. Try .roll d100 or .roll 3d100';
+			const basicPattern = /^([1-9]\d{0,1}|[1][0][0])$/;
+			const dPattern = /^(([1-9]|[1][0]){0,1}d([1-9]\d{0,1}|[1][0][0]))$/;
+			if (args[0].match(basicPattern)) {
+				reply = `:game_die: <@${message.author.id}> rolled ${(Math.floor(Math.random() * args[0]) + 1)} :game_die:`;
+			} else if (args[0].match(dPattern)) {
+				const [quantity, size] = args[0].split('d');
+				if (!quantity) return this.execute(message, [size]);
+				reply = `:game_die: <@${message.author.id}> rolled `;
+				for (let i = 0;i < quantity; i++) {
+					reply += (Math.floor(Math.random() * size) + 1) + ', ';
+				}
+				reply = reply.slice(0, -2) + ' :game_die:';
 			}
+			await message.channel.send(reply, { allowedMentions: { parse: [] } });
 		} catch (error) {
+			message.channel.send('something went wrong!');
 			console.error(error);
 		}
 	},
