7f84bdd31e0d10a7db284f48388c1c85d8432827
THUNDERWANG <crapitsjason@gmail.com>
2021-01-20 17:08:20 -0800

    make roll recursive
    for the luls

--- app/discord/message/commands/roll.js
+++ app/discord/message/commands/roll.js
@@ -6,22 +6,26 @@ module.exports = {
 	cooldown: 3,
 	description: 'generate a number from 1-X',
 	async execute(message, args) {
+
+		function roll(arg) {
+			const less100 = /^([1-9]\d{0,1}|100)$/;
+			const quantDSize = /^(([1-9]|10){0,1}d([1-9]\d{0,1}|100))$/;
+
+			if (arg.match(less100)) return (Math.floor(Math.random() * arg)) + 1;
+			if (arg.match(quantDSize)) {
+				const [quantity, size] = arg.split('d');
+				if (!quantity) return roll(size);
+				let numbers = '';
+				for (let i = 0;i < quantity; i++) { numbers += roll(size) + ', '; }
+				return numbers.slice(0, -2);
+			}
+		}
+
 		try {
 			if (args.length > 1) return;
 			let reply = 'Invalid argument. The max number of rolls is 10 and the max die size is 100. Try .roll d100 or .roll 3d100';
-			const basicPattern = /^([1-9]\d{0,1}|[1][0][0])$/;
-			const dPattern = /^(([1-9]|[1][0]){0,1}d([1-9]\d{0,1}|[1][0][0]))$/;
-			if (args[0].match(basicPattern)) {
-				reply = `:game_die: <@${message.author.id}> rolled ${(Math.floor(Math.random() * args[0]) + 1)} :game_die:`;
-			} else if (args[0].match(dPattern)) {
-				const [quantity, size] = args[0].split('d');
-				if (!quantity) return this.execute(message, [size]);
-				reply = `:game_die: <@${message.author.id}> rolled `;
-				for (let i = 0;i < quantity; i++) {
-					reply += (Math.floor(Math.random() * size) + 1) + ', ';
-				}
-				reply = reply.slice(0, -2) + ' :game_die:';
-			}
+			const numbers = roll(args[0]);
+			if (numbers) reply = `you rolled ${numbers}`;
 			await message.channel.send(reply, { allowedMentions: { parse: [] } });
 		} catch (error) {
 			message.channel.send('something went wrong!');
