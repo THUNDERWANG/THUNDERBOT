bcb65aec1e5e64f846805b88d4d71f90dbc2f477
THUNDERWANG <crapitsjason@gmail.com>
2021-02-03 00:04:48 -0800

    change embed aesthetics
    include author avatar and make sure widest image comes first

--- app/discord/message/webhooks/parseWebhooks.js
+++ app/discord/message/webhooks/parseWebhooks.js
@@ -1,43 +1,47 @@
 const Discord = require('discord.js');
 const { onlineCubeDraftsServerId, mtgaDailyDealChannelId, botId } = require('config').Discord;
+const { loadImage } = require('canvas');
 
 module.exports = async message => {
 	try {
-		if (message.author.username !== 'MTGA Daily Deal' && !message.webHookID) return;
+		if (message.author.username !== 'MTGA Daily Deal' && !message.webHookID && message.content.toLowerCase().includes('here are')) return;
 		const onlineCubeDrafts = await message.client.guilds.fetch(onlineCubeDraftsServerId);
 		const mtgaDailyDealChannel = onlineCubeDrafts.channels.cache.get(mtgaDailyDealChannelId);
 
-		// message.content === Here are the #MTGArena Daily Deals for today! #MTG #MTGA https://t.co/Tb6arU7O7z@@@@@https://pbs.twimg.com/media/EsM6mCLWMAgggmj.jpg
-		let [tweet, media] = message.content.split('@@@@@');
+		// message.content === Here are the #MTGArena Daily Deals for today! #MTG #MTGA https://t.co/Tb6arU7O7z
+		// @@@@@<profile pic>
+		// @@@@@<image1, image2...>
 
-		if (!tweet) tweet = 'Here is the daily MTG Arena content!' + tweet;
+		const [tweet, profilePic, imageStrings] = message.content.split('@@@@@');
+		if (!imageStrings) return;
 		const comment = tweet.slice(0, tweet.lastIndexOf('https://'));
 		const tweetURL = tweet.slice(tweet.lastIndexOf('https://'));
 
+		// create an array of objects with the following fields: url and width
+		const imageUrls = imageStrings.split(',');
+		let images = imageUrls.map(url=>(loadImage(url)));
+		images = await Promise.all(images);
+		images = images.map((image, index)=>({ url: imageUrls[index], width: image.width }));
+		images.sort((a, b) => b.width - a.width);
+
+		// send first image
 		const messageEmbed = new Discord.MessageEmbed()
 			.setColor(message.guild.members.cache.get(botId).roles.highest.color)
-			.setTitle(':mega: MTGA Daily Deal :mega:')
+			.setTitle(':mega: Daily Deal :mega:')
+			.setAuthor('@ArenaDailyDeal', profilePic, tweetURL)
 			.setDescription(comment)
-			.setThumbnail('https://pbs.twimg.com/profile_images/1338516635661918208/N2oDUhgY_400x400.jpg')
-			.setURL(tweetURL);
+			.setThumbnail('https://i.imgur.com/7tVYAeF.png')
+			.setURL(tweetURL)
+			.setImage(images.shift().url);
 
-		// if no images, send message and return
-		if (!media) {
-			const messageResponse = await mtgaDailyDealChannel.send(messageEmbed);
-			return await messageResponse.crosspost();
-		}
-
-		// for the first image, attach image and send embed message
-		media = media.split(',');
-		messageEmbed.setImage(media.shift());
 		const messageResponse = await mtgaDailyDealChannel.send(messageEmbed);
 		await messageResponse.crosspost();
 
 		// send the rest of the images in separate new embed messages
-		for (const extraImage of media) {
+		for (const extraImage of images) {
 			const discordEmbed = new Discord.MessageEmbed()
 				.setColor(message.guild.members.cache.get(botId).roles.highest.color)
-				.setImage(extraImage);
+				.setImage(extraImage.url);
 			const extraEmbeds = await mtgaDailyDealChannel.send(discordEmbed);
 			await extraEmbeds.crosspost();
 		}
