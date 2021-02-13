3c44cc5c4597ef3c2b747ade5693dcae22c543d6
THUNDERWANG <crapitsjason@gmail.com>
2021-02-13 01:30:03 -0800

    remove promise

--- app/discord/handlers/message/middleware/helpers/message.helpers.js
+++ app/discord/handlers/message/middleware/helpers/message.helpers.js
@@ -6,14 +6,14 @@ module.exports = function(message) {
 		// ALL MESSAGES SENT SHOULD NOT MENTION--ESPECIALLY ROLES!
 		noMention: { allowedMentions: { parse: [] } },
 		// always begin the reply with the user's discordTag (BUT DOES NOT ALLOW MENTIONS)
-		replyToAuth: async function(reply) {
-			return await message.channel.send(`<@!${message.author.id}> ${reply}`, message.helpers.noMention);
+		replyToAuth: function(reply) {
+			return message.channel.send(`<@!${message.author.id}> ${reply}`, message.helpers.noMention);
 		},
-		reply: async function(reply) {
-			return await message.channel.send(reply, message.helpers.noMention);
+		reply: function(reply) {
+			return message.channel.send(reply, message.helpers.noMention);
 		},
-		replyEmbed: async function(embed) {
-			return await message.channel.send(embed);
+		replyEmbed: function(embed) {
+			return message.channel.send(embed);
 		},
 
 	};
