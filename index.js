57d9ce0026904149f29ace9587dcfb2862631a12
THUNDERWANG <crapitsjason@gmail.com>
2021-02-28 01:16:26 -0800

    moved login line

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const { botToken } = require('config').discord;
 
 require('./app/logger/logger.js')();
 const discordClient = new Discord.Client();
+discordClient.login(botToken);
 require('./app/discord/handlers/onReady.js')(discordClient);
 require('./app/discord/handlers/onGuild.js')(discordClient);
 require('./app/discord/handlers/onMessage.js')(discordClient);
 
-discordClient.login(botToken);
 module.exports = discordClient;
