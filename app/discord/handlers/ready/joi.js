c8e361424d5623bf560686db90b4dd6fdfe87160
THUNDERWANG <crapitsjason@gmail.com>
2021-03-03 23:13:49 -0800

    fixed cube link validation

--- app/discord/handlers/ready/joi.js
+++ app/discord/handlers/ready/joi.js
@@ -5,6 +5,6 @@ const Joi = require('joi');
 module.exports = () =>{
 	// ensure the URL is from cube cobra/tutor
 	Joi.validateCubeURL = function(url) {
-		return Joi.string().regex(/^https?:\/\/cube(cobra|tutor).com/i).uri().validate(url);
+		return Joi.string().regex(/^https?:\/\/(www.)?cube(cobra|tutor).com/i).uri().validate(url);
 	};
 };
