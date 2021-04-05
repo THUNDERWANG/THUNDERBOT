dfb078568cb029719b01b17ddf5b49997a11d2ac
THUNDERWANG <crapitsjason@gmail.com>
2021-04-05 16:29:18 -0700

    updated fire alias

--- app/cmds/members/fire.js
+++ app/cmds/members/fire.js
@@ -6,6 +6,7 @@ module.exports = class FireCommand extends Commando.Command {
   constructor(client) {
     super(client, {
       name: 'fire',
+      aliases: ['fired'],
       group: 'members',
       guildOnly: true,
       memberName: 'fire',
