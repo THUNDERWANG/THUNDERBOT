fb6c96beda71591d47e68ed8e124659a34be9642
THUNDERWANG <crapitsjason@gmail.com>
2021-04-05 03:07:27 -0700

    temp remove drafters

--- app/cmds/members/drafters.js
+++ app/cmds/members/drafters.js
@@ -1,34 +1,36 @@
-const Commando = require('discord.js-commando');
-const logger = require('@logger/logger.js');
-const { triceRole, xmageRole, draftPlanningChannel } = require('config').discord;
+// const Commando = require('discord.js-commando');
+// const logger = require('@logger/logger.js');
+// const { triceRole, xmageRole, draftPlanningChannel } = require('config').discord;
 
-module.exports = class DrafterCommand extends Commando.Command {
-  constructor(client) {
-    super(client, {
-      name: 'assemble',
-      group: 'members',
-      patterns: [/^<@&/],
-      guildOnly: true,
-      memberName: 'assemble',
-      description: 'Just use @xmage or @cockatrice.',
-      throttling: { usages: 1, duration: 600 },
-    });
-  }
+// module.exports = class DrafterCommand extends Commando.Command {
+//   constructor(client) {
+//     super(client, {
+//       name: 'assemble the legion!',
+//       group: 'members',
+//       patterns: [/^<@&/i, /^@xmage/i, /^@trice/i, /^@cock/i, /^@cockatrice/i],
+//       guildOnly: true,
+//       argsCount: 1,
+//       argsType: 'single',
+//       memberName: 'assemble',
+//       description: 'Type in @xmage or @cockatrice.',
+//       throttling: { usages: 1, duration: 600 },
+//     });
+//   }
 
-  async run(message) {
-    try {
-      if (message.channel.id === draftPlanningChannel) {
-        if (message.content === `<@&${xmageRole}>`) {
-          const mention = await message.say(`<@&${xmageRole}>`);
-          mention.delete();
-        } else if (message.content === `<@&${triceRole}>`) {
-          const mention = await message.say(`<@&${triceRole}>`);
-          mention.delete();
-        }
-      }
-    } catch (error) {
-      message.say('Something went wrong!');
-      logger.error(error);
-    }
-  }
-};
+//   async run(message) {
+//     const content = message.content.trim().toLowerCase().split(' ')[0];
+//     try {
+//       if (message.channel.id === draftPlanningChannel) {
+//         if (content === `<@&${xmageRole}>` || content === '@xmage') {
+//           await message.say(`**<@&${xmageRole}> Assemble!**`);
+
+//         } else if (content === `<@&${triceRole}` || content === '@trice' || content === '@cockatrice' || message.content === '@cock') {
+//           await message.say(`**<@&${triceRole}> Assemble!**`);
+//         }
+//       }
+//     } catch (error) {
+//       message.say('Something went wrong!');
+//       logger.error(error);
+//     }
+//   }
+// };
