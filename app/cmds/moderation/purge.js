dd451155972f2a9364dc0ecd25d16fe7010c0d41
THUNDERWANG <crapitsjason@gmail.com>
2021-04-05 18:30:24 -0700

    fixed that users could purge in announcement channel possible commando bug

--- app/cmds/moderation/purge.js
+++ app/cmds/moderation/purge.js
@@ -1,7 +1,8 @@
 const Commando = require('discord.js-commando');
 const logger = require('@logger/logger.js');
 
-module.exports = class AddCommand extends Commando.Command {
+// Best to implement hasPermission() because user who can post in announcement channels can bypass userPermissions[]
+module.exports = class PurgeCommand extends Commando.Command {
   constructor(client) {
     super(client, {
       name: 'purge',
@@ -9,24 +10,27 @@ module.exports = class AddCommand extends Commando.Command {
       memberName: 'purge',
       description: 'purge up to 20 messages that are less than 14 days old',
       examples: ['.purge 10'],
-      throttling: { usages: 1, duration: 3 },
       userPermissions: ['MANAGE_MESSAGES'],
+      throttling: { usages: 1, duration: 3 },
       args: [
         {
           key: 'amount',
-          prompt: 'Choose an integer less than 20',
+          prompt: 'Choose an int between 1 - 20',
           type: 'integer',
         },
       ],
     });
   }
 
+  hasPermission(message) {
+    const permissions = message.channel.guild.members.cache.get(message.author.id).permissions.toArray();
+    return permissions.includes('MANAGE_MESSAGES');
+  }
+
   async run(message, { amount }) {
     try {
-      if (amount === 0) return;
-      if (amount < 0 || amount > 20) return message.reply('int must be < +20');
-
-      await message.channel.bulkDelete(amount + 1);
+      if (amount <= 0 || amount > 20) return message.reply('int must be between 1 - 20');
+      message.channel.bulkDelete(amount + 1);
     } catch (error) {
       message.say(error.message);
       if (error.message === 'Missing Permissions') logger.error(message.author.tag);
