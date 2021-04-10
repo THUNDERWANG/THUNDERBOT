35b091c06bb9d5e62b5d20eeefad4dd71ce03d4d
THUNDERWANG <crapitsjason@gmail.com>
2021-04-10 14:40:15 -0700

    refactored weekly winner

--- app/features/features.js
+++ app/features/features.js
@@ -1,9 +1,11 @@
 const setStatus = require('@features/status/setStatus.js');
+const setWeeklyWinner = require('@features/webhooks/setWeeklyWinner.js');
 const setDailyDeal = require('@features/webhooks/setDailyDeal.js');
 const setGerm = require('@features/welcome/setGerm.js');
 
 module.exports = (client) => {
   setDailyDeal(client);
+  setWeeklyWinner(client);
   setStatus(client);
   setGerm(client);
 };
--- app/features/webhooks/setWeeklyWinner.js
+++ app/features/webhooks/setWeeklyWinner.js
@@ -2,21 +2,22 @@ const { serverId, offTopicChannel } = require('config').discord;
 const logger = require('@logger/logger.js');
 const { zapier } = require('config').discord;
 
+// TODO: Refactor so text is filtered by message.embeds
 module.exports = async (client) => {
   logger.info('Listening for Weekly Winners...');
   client.on('message', async (message) => {
     try {
-      if (message.author.id !== zapier && !message.content.toLowerCase().includes('weekly winners')) return;
+      if (message.author.id !== zapier) return;
       const onlineCubeDrafts = await message.client.guilds.fetch(serverId);
       const offTopic = onlineCubeDrafts.channels.cache.get(offTopicChannel);
-
-      // parse message (see above for example)
-      const url = message.content.split('@@@@@')[0];
-      if (url) await offTopic.send(url);
+      const target = message.embeds.find((embed) => embed.description.toLowerCase().includes('weekly winners'));
+      if (target) {
+        target.image = null;
+        await offTopic.send(target);
+      }
 
     } catch (error) {
       logger.error(error);
     }
   });
-
 };
