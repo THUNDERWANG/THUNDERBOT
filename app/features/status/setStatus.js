2e83e25d2801aa5a57850bbf0eba4a488c57a161
Jason Wang <jasonywang0@gmail.com>
2021-05-22 01:50:07 -0700

    added marit lages

--- app/features/status/setStatus.js
+++ app/features/status/setStatus.js
@@ -1,20 +1,24 @@
 const logger = require('@logger/logger.js');
 
+// TODO: make an array of user ids and look up current username; cycle through array instead of random
 const statuses = [];
-statuses.push('Esper Control');
-statuses.push('Mono Red');
-statuses.push('UB Reanimator');
-statuses.push('Bant Midrange');
-statuses.push('Orzhov Aggro');
+statuses.push('THUNDERWANG');
+statuses.push('The_Steve_Man');
+statuses.push('MapcasterSnage');
+statuses.push('ikzann');
+statuses.push('domcosta');
+statuses.push('tettsui the local drunk');
+statuses.push('Lonestar4729');
+statuses.push('Purplemurasaki');
 
 module.exports = async function setStatus(client) {
   try {
     const randomStatus = statuses[Math.floor((Math.random() * statuses.length))];
-    await client.user.setActivity(randomStatus);
+    await client.user.setActivity(`vs ${randomStatus}`);
     logger.info(`Status set: ${randomStatus}`);
     setTimeout(() => {
       setStatus(client);
-    }, (10800000));
+    }, (3600000));
   } catch (error) {
     logger.error(error);
   }
