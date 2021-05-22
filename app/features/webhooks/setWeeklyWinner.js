888e56a38c39316201fe0b2592edc0b230e65951
Jason Wang <jasonywang0@gmail.com>
2021-05-22 01:20:37 -0700

    removed weekly winner feature

--- app/features/webhooks/setWeeklyWinner.js
+++ app/features/webhooks/setWeeklyWinner.js
@@ -1,23 +1,24 @@
-const { serverId, offTopicChannel } = require('config').discord;
-const logger = require('@logger/logger.js');
-const { zapier } = require('config').discord;
+// const { serverId, offTopicChannel } = require('config').discord;
+// const logger = require('@logger/logger.js');
+// const { zapier } = require('config').discord;
 
-// TODO: Refactor so text is filtered by message.embeds
-module.exports = (client) => {
-  logger.info('Listening for Weekly Winners...');
-  client.on('message', async (message) => {
-    try {
-      if (message.author.id !== zapier) return;
-      const onlineCubeDrafts = await message.client.guilds.fetch(serverId);
-      const offTopic = onlineCubeDrafts.channels.cache.get(offTopicChannel);
-      const target = message.embeds.find((embed) => embed.description.toLowerCase().includes('weekly winners'));
-      if (target) {
-        target.image = null;
-        await offTopic.send(target);
-      }
+// // TODO: Refactor so text is filtered by message.embeds
+// // Currently disabled for lack of interest and instability
+// module.exports = (client) => {
+//   logger.info('Listening for Weekly Winners...');
+//   client.on('message', async (message) => {
+//     try {
+//       if (message.author.id !== zapier) return;
+//       const onlineCubeDrafts = await message.client.guilds.fetch(serverId);
+//       const offTopic = onlineCubeDrafts.channels.cache.get(offTopicChannel);
+//       const target = message.embeds.find((embed) => embed.description.toLowerCase().includes('weekly winners'));
+//       if (target) {
+//         target.image = null;
+//         await offTopic.send(target);
+//       }
 
-    } catch (error) {
-      logger.error(error);
-    }
-  });
-};
+//     } catch (error) {
+//       logger.error(error);
+//     }
+//   });
+// };
