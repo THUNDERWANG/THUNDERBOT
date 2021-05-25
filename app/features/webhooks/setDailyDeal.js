d0779b6f7785c47f80a211a06801d3fdc6373e0f
Jason Wang <jasonywang0@gmail.com>
2021-05-25 01:45:02 -0700

    fixed MTGA broadcasts messages not filtered correctly

--- app/features/webhooks/setDailyDeal.js
+++ app/features/webhooks/setDailyDeal.js
@@ -18,43 +18,44 @@ module.exports = (client) => {
   logger.info('Listening for deals...');
   client.on('message', async (message) => {
     try {
-      if (message.author.id !== zapier && !message.content.toLowerCase().includes('here are')) return;
-      const onlineCubeDrafts = await message.client.guilds.fetch(serverId);
-      const mtgaDailyDealChannel = onlineCubeDrafts.channels.cache.get(mtgaChannelId);
+      if (message.author.id === zapier && message.content.toLowerCase().includes('here are')) {
+        const onlineCubeDrafts = await message.client.guilds.fetch(serverId);
+        const mtgaDailyDealChannel = onlineCubeDrafts.channels.cache.get(mtgaChannelId);
 
-      // parse message (see above for example)
-      const [tweet, profilePic, imageStrings] = message.content.split('@@@@@');
-      if (!imageStrings) return;
-      const comment = tweet.slice(0, tweet.lastIndexOf('https://'));
-      const tweetURL = tweet.slice(tweet.lastIndexOf('https://'));
+        // parse message (see above for example)
+        const [tweet, profilePic, imageStrings] = message.content.split('@@@@@');
+        if (!imageStrings) return;
+        const comment = tweet.slice(0, tweet.lastIndexOf('https://'));
+        const tweetURL = tweet.slice(tweet.lastIndexOf('https://'));
 
-      // create an array of objects with the following fields: url and width
-      const imageUrls = imageStrings.split(',');
-      let images = imageUrls.map((url) => loadImage(url));
-      images = await Promise.all(images);
-      images = images.map((image, index) => ({ url: imageUrls[index], width: image.width }));
-      images.sort((a, b) => b.width - a.width);
+        // create an array of objects with the following fields: url and width
+        const imageUrls = imageStrings.split(',');
+        let images = imageUrls.map((url) => loadImage(url));
+        images = await Promise.all(images);
+        images = images.map((image, index) => ({ url: imageUrls[index], width: image.width }));
+        images.sort((a, b) => b.width - a.width);
 
-      // send first image
-      const messageEmbed = createEmbed()
-        .setColor('#1DA1F2')
-        .setAuthor('@ArenaDailyDeal', profilePic, tweetURL)
-        .setDescription(comment)
-        .setThumbnail('https://i.imgur.com/7tVYAeF.png')
-        .setURL(tweetURL)
-        .setImage(images.shift().url);
-
-      const messageResponse = await mtgaDailyDealChannel.send(messageEmbed);
-      await messageResponse.crosspost();
-
-      // send the rest of the images in separate new embed messages
-      for (const extraImage of images) {
-        const discordEmbed = createEmbed()
-          .setThumbnail(null)
+        // send first image
+        const messageEmbed = createEmbed()
           .setColor('#1DA1F2')
-          .setImage(extraImage.url);
-        const extraEmbeds = await mtgaDailyDealChannel.send(discordEmbed);
-        await extraEmbeds.crosspost();
+          .setAuthor('@ArenaDailyDeal', profilePic, tweetURL)
+          .setDescription(comment)
+          .setThumbnail('https://i.imgur.com/7tVYAeF.png')
+          .setURL(tweetURL)
+          .setImage(images.shift().url);
+
+        const messageResponse = await mtgaDailyDealChannel.send(messageEmbed);
+        await messageResponse.crosspost();
+
+        // send the rest of the images in separate new embed messages
+        for (const extraImage of images) {
+          const discordEmbed = createEmbed()
+            .setThumbnail(null)
+            .setColor('#1DA1F2')
+            .setImage(extraImage.url);
+          const extraEmbeds = await mtgaDailyDealChannel.send(discordEmbed);
+          await extraEmbeds.crosspost();
+        }
       }
     } catch (error) {
       logger.error(error);
