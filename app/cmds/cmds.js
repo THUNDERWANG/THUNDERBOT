0f5adddab02ab95136c0795ab3d0975de55aa466
jasonywang0 <jasonywang0@gmail.com>
2021-07-13 00:52:09 -0700

    turned off unknown command detection

--- app/cmds/cmds.js
+++ app/cmds/cmds.js
@@ -2,10 +2,12 @@ const path = require('path');
 
 module.exports = (client) => {
   client.registry
-    .registerDefaults()
+    .registerDefaultTypes()
+    .registerDefaultGroups()
     .registerGroups([
       ['members', 'Member commands'],
       ['moderation', 'Moderation commands'],
     ])
+    .registerDefaultCommands({ unknownCommand: false })
     .registerCommandsIn(path.join(__dirname));
 };
